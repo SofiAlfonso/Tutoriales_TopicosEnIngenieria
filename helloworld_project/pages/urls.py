19833cefd916fe670a9841e1ea5462ed3cb6abd5
anasofalfons_meli <anasofia.alfonso@mercadolibre.com.co>
2025-08-06 07:07:12 -0500

    ejercicio clase 2 - lista de productos

--- helloworld_project/pages/urls.py
+++ helloworld_project/pages/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .views import HomePageView, AboutPageView, ProductIndexView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('contact/', ContactPageView.as_view(), name='contact'),
     path('products/', ProductIndexView.as_view(), name='index'),
-    path('products/create/', ProductCreateView.as_view(), name='form'),
-    path('products/<str:id>/', ProductShowView.as_view(), name='show'),
 ]
--- helloworld_project/pages/views.py
+++ helloworld_project/pages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views import View
 from django import forms
@@ -26,26 +26,12 @@ class AboutPageView(TemplateView):
             })
         return context
 
-class ContactPageView(TemplateView):
-    template_name = 'pages/contact.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            "title": "How can you find us?",
-            "subtitle": "our contact",
-            "email": "email: contactme@gmail.com",
-            "phone_number": "tel: 3887756",
-            "address": "dir: Cra Sofi #22-81"
-            })
-        return context
-
 class Product:
     products = [
-        {"id":"1", "name":"TV", "description":"Best TV", "price":4000000},
-        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":1000000},
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":400000},
-        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":300000}
+        {"id":"1", "name":"TV", "description":"Best TV", "price":4000000, "img": "../static/products/img/tv.png"},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":1000000, "img": "../static/products/img/iphone.png"},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":400000, "img": "../static/products/img/chromecast.png"},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":300000, "img": "../static/products/img/glasses.png"}
     ]
 
 class ProductIndexView(View):
@@ -57,54 +43,3 @@ class ProductIndexView(View):
         viewData["products"] = Product.products
         return render(request, self.template_name, viewData)
 
-class ProductShowView(View):
-    template_name = 'products/show.html'
-
-    def get(self, request, id):
-        viewData = {}
-        try:
-            product = Product.products[int(id)-1]
-        except:
-            return redirect('home')
-        viewData["title"] = product["name"] + " - Online Store"
-        viewData["subtitle"] = product["name"] + " - Product information"
-        viewData["product"] = product
-
-        return render(request, self.template_name, viewData)
-
-class ProductForm(forms.Form):
-    name = forms.CharField(required=True)
-    price = forms.FloatField(required=True)
-
-    def clean_price(self):
-        price = self.cleaned_data.get('price')
-        if price is None:
-            return price
-        if price <= 0:
-            raise forms.ValidationError("El precio debe ser mayor que cero.")
-        return price
-
-class ProductCreateView(View):
-    template_success= 'products/success.html'
-    template_name = 'products/create.html'
-
-    def get(self, request):
-        form = ProductForm()
-        viewData = {}
-        viewData["title"] = "Create product"
-        viewData["form"] = form
-        return render(request, self.template_name, viewData)
-    
-    def post(self, request):
-        form = ProductForm(request.POST)
-        if form.is_valid():
-              return render(request, self.template_success, {
-                "title": "Product created",
-                "message": "Product created",
-                "product": Product.products[-1]
-            })
-        else:
-            viewData = {}
-            viewData["title"] = "Create product"
-            viewData["form"] = form
-            return render(request, self.template_name, viewData)
