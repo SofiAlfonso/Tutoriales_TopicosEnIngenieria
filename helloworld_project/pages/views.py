b5f4f1454638653f6628b689812229896581c84e
anasofalfons_meli <anasofia.alfonso@mercadolibre.com.co>
2025-08-06 01:18:25 -0500

    add: final activities for first tutorial

--- helloworld_project/pages/views.py
+++ helloworld_project/pages/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.views import View
+from django import forms
 #from django.http import HttpResponse
 
 # Create your views here.
@@ -41,10 +42,10 @@ class ContactPageView(TemplateView):
 
 class Product:
     products = [
-        {"id":"1", "name":"TV", "description":"Best TV"},
-        {"id":"2", "name":"iPhone", "description":"Best iPhone"},
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
-        {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+        {"id":"1", "name":"TV", "description":"Best TV", "price":4000000},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":1000000},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":400000},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":300000}
     ]
 
 class ProductIndexView(View):
@@ -61,9 +62,49 @@ class ProductShowView(View):
 
     def get(self, request, id):
         viewData = {}
-        product = Product.products[int(id)-1]
+        try:
+            product = Product.products[int(id)-1]
+        except:
+            return redirect('home')
         viewData["title"] = product["name"] + " - Online Store"
         viewData["subtitle"] = product["name"] + " - Product information"
         viewData["product"] = product
 
         return render(request, self.template_name, viewData)
+
+class ProductForm(forms.Form):
+    name = forms.CharField(required=True)
+    price = forms.FloatField(required=True)
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price is None:
+            return price
+        if price <= 0:
+            raise forms.ValidationError("El precio debe ser mayor que cero.")
+        return price
+
+class ProductCreateView(View):
+    template_success= 'products/success.html'
+    template_name = 'products/create.html'
+
+    def get(self, request):
+        form = ProductForm()
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] = form
+        return render(request, self.template_name, viewData)
+    
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+              return render(request, self.template_success, {
+                "title": "Product created",
+                "message": "Product created",
+                "product": Product.products[-1]
+            })
+        else:
+            viewData = {}
+            viewData["title"] = "Create product"
+            viewData["form"] = form
+            return render(request, self.template_name, viewData)
